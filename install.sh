a59207f38d8333e6a2484692010df84e8dbd6d44
Alvin82 <48894530+Alvin82@users.noreply.github.com>
2022-02-22 20:26:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-ROOT=~/.certbot-src
+DIR=/opt/certbot-src
 
-# Remove older versions if existing, for a simple re-install/update.
-rm -rf $ROOT
-
-# Creating the directory
-mkdir -p $ROOT
-
-# Clone certbot source.
-git clone https://github.com/certbot/certbot $ROOT
-
-# Check if python3-venv is installed, and install it otherwise.
-CHECK_PACKAGES='python3-venv git'
+# Check if packages isn't installed, then install it.
+CHECK_PACKAGES='git python3-dev python3-venv gcc libaugeas0 libssl-dev libffi-dev ca-certificates openssl'
 if ! dpkg -s $CHECK_PACKAGES >/dev/null 2>&1; then
   echo "The packages '$CHECK_PACKAGES' are not installed. "
   echo "They will now be installed automatically with 'sudo apt-get install $CHECK_PACKAGES'."
-  sudo apt-get install -y $CHECK_PACKAGES
+  apt-get install -y $CHECK_PACKAGES
 fi
 
+# Remove older versions if existing, for a simple re-install/update.
+rm -rf $DIR
+
+# Creating the directory
+mkdir -p $DIR
+
+# Clone certbot source.
+git clone https://github.com/certbot/certbot $DIR
+
 # Setup virtual environment for certbot.
-cd $ROOT
-sudo apt-get install -y python3-dev python3-venv gcc libaugeas0 libssl-dev libffi-dev ca-certificates openssl
+cd $DIR
 python3 tools/venv.py
 
 # Link installed certbot version, so it can used everywhere.
-sudo ln -s $ROOT/venv/bin/certbot /usr/local/bin/
+ln -s $DIR/venv/bin/certbot /usr/local/bin/
